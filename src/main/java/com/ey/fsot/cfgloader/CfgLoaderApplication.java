0df05b9653685881399f465a1a305eed7a43370e
nathan.g.vella@ey.com <nathan.g.vella@ey.com-at-033203213788>
2020-04-27 13:16:19 -0400

    initial commit

---
+++ src/main/java/com/ey/fsot/cfgloader/CfgLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.ey.fsot.cfgloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfgLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfgLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ey/fsot/cfgloader/CfgLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ey.fsot.cfgloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfgLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
